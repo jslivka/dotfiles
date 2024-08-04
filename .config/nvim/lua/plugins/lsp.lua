dc087ee7fd4b401eaedd740e3b482d8ff14be059
John Slivka <john@jslivka.net>
2024-08-04 18:25:23 -0500

    add doom emacs configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
         end,
       },
       { "williamboman/mason-lspconfig.nvim" }, -- Optional
-  
+
       -- Conjure (for lisp variants)
       {
         "Olical/conjure",
@@ -36,8 +36,8 @@ return {
         end,
       },
       -- Other lisp plugins
-      { 
-        'benknoble/vim-racket' 
+      {
+        'benknoble/vim-racket'
       },
 
       -- Autocompletion
@@ -52,10 +52,10 @@ return {
     },
     config = function()
       local lsp = require("lsp-zero")
-  
+
       lsp.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
-  
+
         vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Reference" }))
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Definition" }))
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Hover" }))
@@ -68,7 +68,7 @@ return {
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Rename" }))
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Signature Help" }))
       end)
-  
+
       require("mason").setup({})
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -88,13 +88,13 @@ return {
           end,
         },
       })
-  
+
       local cmp_action = require("lsp-zero").cmp_action()
       local cmp = require("cmp")
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
-  
+
       require("luasnip.loaders.from_vscode").lazy_load()
-  
+
       -- `/` cmdline setup.
       cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
@@ -102,7 +102,7 @@ return {
           { name = "buffer" },
         },
       })
-  
+
       -- `:` cmdline setup.
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
@@ -117,7 +117,7 @@ return {
           },
         }),
       })
-  
+
       cmp.setup({
         snippet = {
           expand = function(args)
