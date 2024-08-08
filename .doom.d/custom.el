33f5fa8a7648d390742014fda4cd71b688374d22
John Slivka <slivkajb@gmail.com>
2024-08-08 11:11:38 -0500

    add lisp configs, treemacs

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(racket-mode vterm lsp-haskell)))
+ '(package-selected-packages
+   '(all-the-icons lsp-haskell racket-mode rotate sly-quicklisp vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
