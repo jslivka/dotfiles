fcaeadcebb583f5a986006e338591ffcbade1e42
John Slivka <john@jslivka.net>
2024-08-30 14:02:42 -0500

    zen mode config

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -1,3 +1,12 @@
 return {
   "folke/zen-mode.nvim",
+  cmd = "ZenMode",
+  opts = {
+    plugins = {
+      gitsigns = true,
+    },
+  },
+  init = function()
+    vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "Zen Mode", silent = true })
+  end,
 }
