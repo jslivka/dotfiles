6dfa22db1ffd20f0d001094d6fd63d1f1083c551
John Slivka <john@jslivka.net>
2025-02-09 13:28:11 -0600

    add keymaps, fzf

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local builtin = require("telescope.builtin")
+local map = vim.keymap.set
+
+-- Map leader to <Space>
+map("n", " ", "<Nop>", { silent = true, remap = false })
+vim.g.mapleader = " "
+
+-- Telescope
+map("n", "<leader>ff", builtin.find_files, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fb", builtin.buffers, {})
+map("n", "<leader>fh", builtin.help_tags, {})
