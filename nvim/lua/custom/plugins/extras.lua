987ce997b3283c915f7d1d1437e45e71d1e18a34
John Slivka <jslivka@splunk.com>
2025-02-10 09:46:00 -0600

    remove typescript stuff

--- nvim/lua/custom/plugins/extras.lua
+++ nvim/lua/custom/plugins/extras.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
