18d28ae98d4d880098bc97804c14e85ff17a4541
John Slivka <john@jslivka.net>
2025-06-29 10:44:46 -0500

    switch from nvim cmp to blink, add more features

--- nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -7,6 +7,25 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        harper_ls = {
+          enabled = true,
+          filetypes = { "markdown" },
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = "~/github/dotfiles-latest/neovim/neobean/spell/en.utf-8.add",
+              linters = {
+                ToDoHyphen = false,
+                SpellCheck = true,
+              },
+              isolateEnglish = true,
+              markdown = {
+                -- [ignores this part]()
+                -- [[ also ignores my marksman links ]]
+                IgnoreLinkTitle = true,
+              },
+            },
+          },
+        },
       },
     },
   },
---
+++ nvim/lua/custom/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  cond = vim.g.neovide == nil,
+  opts = {
+    stiffness = 0.8,
+    trailing_stiffness = 0.4,
+    stiffness_insert_mode = 0.6,
+    trailing_stiffness_insert_mode = 0.6,
+    distance_stop_animating = 0.5,
+  },
+}
